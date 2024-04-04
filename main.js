09058149254ad7fc88da3afd23180131b3470b5f
root <root@SURFACE.>
2024-04-04 13:35:21 +0300

    liker

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded",()=>{
+  const emptyHearts = document.querySelectorAll(".like-glyph");
 
+emptyHearts.forEach(hearts => {
+  hearts.addEventListener("click",async(event)=>{
+      try{
+          await mimicServerCall()
+
+          event.target.innerText=FULL_HEART
+          event.target.classList.add("activated-heart");
+      }catch(error){
+              const errorModal = document.getElementById("modal")
+              const errorMessage =document.getElementById("modal-message")
+              errorMessage.textContent = "error"
+              errorModal.classList.remove("activated-heart")
+      
+      setTimeout((errorModal)=>{
+        errorModal.classList.add("hidden")},3000);
+      }
+  })
+});
+const fullHearts= document.querySelectorAll(".like-glyph")
+fullHearts.forEach(hearts =>{
+hearts.addEventListener("click",(event)=>{
+  event.target.innerText = EMPTY_HEART
+  event.target.classList.remove("actiated-heart")
+
+})
+})
+})
+
+
+function hideModal() {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+}
 
 
 
